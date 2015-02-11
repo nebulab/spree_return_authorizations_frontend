7312a09e61620c38d4753ae573f5bf3cfdbbc3a1
Alberto Vena <kennyadsl@gmail.com>
2015-02-11 18:03:34 +0100

    adds return link into order page

--- app/overrides/show_order_return_link.rb
+++ app/overrides/show_order_return_link.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path: "spree/orders/show",
   name: "show_order_return_link",
   insert_bottom: "[data-hook='links']",
-  text: "<%= link_to \"Ask return\", new_return_order_path(@order), class: 'button' %>",
-  disabled: true
+  partial: 'spree/orders/return_authorizations_link'
 )
---
+++ spec/features/spree/orders_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'Order show', type: :feature do
+  let(:user)  { create(:user) }
+  let(:order) { create(:order_ready_to_ship, user: user) }
+  stub_authorization!
+
+  context 'when order is not yet shipped' do
+    it 'has no link to return items' do
+      visit spree.order_path(order)
+      expect(page).to_not have_link Spree.t(:create_returns)
+    end
+  end
+
+  context 'when order has been shipped' do
+    before do
+      order.shipments.each do |shipment|
+        shipment.inventory_units.update_all state: 'shipped'
+        shipment.update_column('state', 'shipped')
+      end
+      order.reload
+    end
+
+    it 'has a link to return items' do
+      visit spree.order_path(order)
+      expect(page).to have_link Spree.t(:create_returns)
+    end
+  end
+end
