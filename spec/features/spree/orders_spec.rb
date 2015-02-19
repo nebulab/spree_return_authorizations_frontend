939afbcbdd8c02f7bb3597a1673b03ed8caa147e
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-02-19 12:30:17 +0100

    fixes hiding return button for guest orders

--- spec/features/spree/orders_spec.rb
+++ spec/features/spree/orders_spec.rb
@@ -1,29 +1,40 @@
 require 'spec_helper'
 
 describe 'Order show', type: :feature do
-  let(:user)  { create(:user) }
-  let(:order) { create(:order_ready_to_ship, user: user) }
-  stub_authorization!
+  context 'when user is authenticated' do
+    let(:user)  { create(:user) }
+    let(:order) { create(:order_ready_to_ship, user: user) }
+    stub_authorization!
 
-  context 'when order is not yet shipped' do
-    it 'has no link to return items' do
-      visit spree.order_path(order)
-      expect(page).to_not have_link Spree.t("return_authorizations_frontend.create_returns")
+    context 'when order is not yet shipped' do
+      it 'has no link to return items' do
+        visit spree.order_path(order)
+        expect(page).to_not have_link Spree.t("return_authorizations_frontend.create_returns")
+      end
+    end
+
+    context 'when order has been shipped' do
+      before do
+        order.shipments.each do |shipment|
+          shipment.inventory_units.update_all state: 'shipped'
+          shipment.update_column('state', 'shipped')
+        end
+        order.reload
+      end
+
+      it 'has a link to return items' do
+        visit spree.order_path(order)
+        expect(page).to have_link Spree.t("return_authorizations_frontend.create_returns")
+      end
     end
   end
 
-  context 'when order has been shipped' do
-    before do
-      order.shipments.each do |shipment|
-        shipment.inventory_units.update_all state: 'shipped'
-        shipment.update_column('state', 'shipped')
-      end
-      order.reload
-    end
+  context 'when is a guest order' do
+    let(:order) { create(:shipped_order, user: nil, email: 'foo@bar.it') }
 
-    it 'has a link to return items' do
-      visit spree.order_path(order)
-      expect(page).to have_link Spree.t("return_authorizations_frontend.create_returns")
+    it 'has no link to return items' do
+      visit spree.token_order_path(order, order.guest_token)
+      expect(page).to_not have_link Spree.t("return_authorizations_frontend.create_returns")
     end
   end
 end
