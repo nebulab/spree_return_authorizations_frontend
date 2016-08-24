b0f961bf46d133986f8f4fec80482b4f750ced7a
SamuelMartini <samueljmartini@gmail.com>
2016-08-24 15:18:03 +0200

    Change return authorization override edit
    Add authorization button just to return authorization edit

--- app/overrides/admin_show_authorize_return_button.rb
+++ app/overrides/admin_show_authorize_return_button.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  virtual_path: "spree/admin/shared/_content_header",
+  virtual_path: "spree/admin/return_authorizations/edit",
   name: "admin_show_authorize_return_button",
-  insert_top: "[data-hook='toolbar'] > ul",
-  text: "<% if @return_authorization.try(:pending?) %><li><%= button_link_to Spree.t('actions.authorize'), fire_admin_order_return_authorization_url(@order, @return_authorization, e: 'authorize'), method: :put, data: { confirm: Spree.t(:are_you_sure) }, :icon => 'ok' %></li><% end %>"
+  insert_before: "erb[loud]:contains('button_link_to Spree.t(:back)')",
+  text: "<% if @return_authorization.try(:pending?) %><%= button_link_to Spree.t('actions.authorize'), fire_admin_order_return_authorization_url(@order, @return_authorization, e: 'authorize'), method: :put, data: { confirm: Spree.t(:are_you_sure) }, :icon => 'ok' %><% end %>"
 )
